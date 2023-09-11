8ad7065b1fa38f4ede77fef4c452b01a2894f24a
Bhavya Vikas <bhavya.vikas2010@gmail.com>
2023-09-11 19:37:24 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-// const heading = React.createElement("h1", { id: "heading" }, "Hello from React");
-const parent = React.createElement(
-    "div", { id: "parent" },
-    [React.createElement("div", { id: "child" },
-        [React.createElement("h1", { id: "innerH1" },
-            "Parent > Child > H1"),
-        React.createElement("h2", { id: "innerH1" },
-            "Parent > Child > H2")]),
-    React.createElement("div", { id: "child2" },
-        [React.createElement("h1", { id: "innerH1" },
-            "Parent > Child > H1"),
-        React.createElement("h2", { id: "innerH1" },
-            "Parent > Child > H2")])
-    ])
+// const heading = React.createElement("h1", { id: "heading" }, "Namaste React 🚀🚀")
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(heading)
+const title =(
+<h2 className="title">Title inside Heading</h2>
+)
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);  
+const number=100;
+const HeadingComponent = ()=>{
+    return (
+        <div>
+    <h1>Namaste React</h1>
+        {title}
+        <h3>{number}</h3>
+        <h3>{100+200}</h3>
+        <h3>{console.log("1A2B3C")}</h3>
+    </div>
+    )
+}
+
+const jsxHeading = (
+<h1 className="heading"
+>Hello🚀👐</h1>
+);
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<HeadingComponent/>)
